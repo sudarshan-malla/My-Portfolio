ed512c6d8fb0ed749356510923961bb35af6cca0
sudarshan-malla <sudarshan.malla75@gmail.com>
2025-01-07 12:31:13 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.nav-links a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const targetId = this.getAttribute('href').substring(1);
-      document.getElementById(targetId).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
